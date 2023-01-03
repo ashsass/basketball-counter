b5665628197d74a98fc5b251fe93d7e729c218ee
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-01-03 17:30:45 -0500

    Add files via upload

---
+++ basketball-counter/index.js
@@ -0,0 +1,34 @@
+let homeEl = document.getElementById("home-score");
+let guestEl = document.getElementById("guest-score");
+let homeScore = 0;
+let guestScore = 0;
+
+function add1Home() {
+    homeScore += 1;
+    homeEl.textContent = homeScore;
+}
+
+function add2Home() {
+    homeScore += 2;
+    homeEl.textContent = homeScore;
+}
+
+function add3Home() {
+    homeScore += 3;
+    homeEl.textContent = homeScore;
+}
+
+function add1Guest() {
+    guestScore += 1;
+    guestEl.textContent = guestScore;
+}
+
+function add2Guest() {
+    guestScore += 2;
+    guestEl.textContent = guestScore;
+}
+
+function add3Guest() {
+    guestScore += 3;
+    guestEl.textContent = guestScore;
+}
